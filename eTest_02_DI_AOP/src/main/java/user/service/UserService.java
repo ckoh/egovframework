3c1f2a8305ced9268de462df0a0315f15d1efba7
ckoh <ckoh@proits.co.kr>
2015-12-05 17:58:09 +0900

    # ckoh : 정리

--- eTest_02_DI_AOP/src/main/java/user/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package user.service;
-
-import user.vo.UserVO;
-
-public interface UserService {
-   public UserVO login(String id,String pw);
-}
--- eTest_03_DB/src/main/java/user/dao/UserDAO.java
+++
@@ -1,7 +0,0 @@
-package user.dao;
-
-import user.vo.UserVO;
-
-public interface UserDAO {
-	   public UserVO login(String id,String pw);
-}
